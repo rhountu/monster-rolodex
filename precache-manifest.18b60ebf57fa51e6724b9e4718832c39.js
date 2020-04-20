8c6491dfe59ead11ba988ed5967f0d4dec00a027
rhountu <rahirawat95@gmail.com>
2020-04-20 10:56:52 +0530

    Updates

---
+++ precache-manifest.18b60ebf57fa51e6724b9e4718832c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e328862be192970ee35c4304689f25ac",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "733414164e104b269d86",
+    "url": "/monster-rolodex/static/css/main.35a59a59.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monster-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733414164e104b269d86",
+    "url": "/monster-rolodex/static/js/main.8fd55ca8.chunk.js"
+  },
+  {
+    "revision": "e4eba3394aec38fdb39c",
+    "url": "/monster-rolodex/static/js/runtime-main.2047114a.js"
+  }
+]);
